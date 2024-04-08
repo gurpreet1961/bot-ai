0443d626cc9c55ca335c9c774db0d4ddad1c082f
Gurpreet Singh <gurpreet012guru@gmail.com>
2024-04-08 20:44:57 +0530

    chat Area frontEnd

---
+++ src/Components/ChatComp.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import icon from "../Assests/Icon.png";
+import "./ChatComp.css";
+const ChatComp = () => {
+	return (
+		<div className="ChatContainer">
+			<h3>Bot AI</h3>
+			<div className="chatArea">
+				<div className="newChat">
+					<div className="heading">
+						<h3>How Can I help You Today?</h3>
+						<img src={icon} alt="" />
+					</div>
+					<div className="demoQuestions">
+						<div className="question">
+							<h4>hi,What is the Weather</h4>
+							<p>Get immediate AI generated respone</p>
+						</div>
+						<div className="question">
+							<h4>hi,What is the Weather</h4>
+							<p>Get immediate AI generated respone</p>
+						</div>
+						<div className="question">
+							<h4>hi,What is the Weather</h4>
+							<p>Get immediate AI generated respone</p>
+						</div>
+						<div className="question">
+							<h4>hi,What is the Weather</h4>
+							<p>Get immediate AI generated respone</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="ask">
+				<input type="text" name="" id="" />
+				<button>Ask</button>
+				<button>Save</button>
+			</div>
+		</div>
+	);
+};
+
+export default ChatComp;
