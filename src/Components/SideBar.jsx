37fbf77134380daf9a14d668f8ce3bf3461b3477
Gurpreet Singh <gurpreet012guru@gmail.com>
2024-04-08 21:47:17 +0530

    added side bar

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -1,17 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import icon from "../Assests/Icon.png";
 import edit from "../Assests/edit.png";
 import "./SideBar.css";
+
 const SideBar = () => {
+	const [showSidebar, setShowSidebar] = useState(false);
+
+	const toggleSidebar = () => {
+		setShowSidebar(!showSidebar);
+	};
+
 	return (
-		<div className="aside">
-			<div className="profile-container">
-				<img src={icon} alt="icon" className="icon" />
-				<h3>New Chat</h3>
-				<img src={edit} alt="" />
+		<div>
+			<div className={`aside ${showSidebar ? "show" : ""}`}>
+				<div className="profile-container">
+					<img src={icon} alt="icon" className="icon" />
+					<h3>New Chat</h3>
+					<img src={edit} alt="" />
+				</div>
+				<div className="pastBtn">
+					<h4>Past Conversations</h4>
+				</div>
 			</div>
-			<div className="pastBtn">
-				<h4>Past Conversations</h4>
+			<div className="hamburger" onClick={toggleSidebar}>
+				<div className="line"></div>
+				<div className="line"></div>
+				<div className="line"></div>
 			</div>
 		</div>
 	);
